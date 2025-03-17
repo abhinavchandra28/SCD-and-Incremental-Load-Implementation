49b746644aaec0b45cdbb25922271d19150067cb
Abhinav Chandra <65914399+abhinavchandra28@users.noreply.github.com>
2025-03-17 23:55:41 +0530

    Update create_table.sql

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE customer_dim (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    city VARCHAR(255),
+    current_job VARCHAR(255),
+    previous_job VARCHAR(255),
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    is_active BOOLEAN
+);
